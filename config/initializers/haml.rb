a39065a9fb3896750ca1a6b6a9682e2994000a79
Matthew MacLeod <matt@matt-m.co.uk>
2011-07-29 19:51:08 +0100

    Added basic template

--- config/initializers/haml.rb
+++
@@ -1,3 +0,0 @@
-Sass::Plugin.options[:syntax] = :sass
-Sass::Plugin.options[:template_location] = File.join(Rails.root, 'app', 'sass')
-Sass::Plugin.options[:style] = Rails.env=="development" ? :expanded : :compressed
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,13 @@ FestSchedule::Application.routes.draw do
   
   root :to => "welcome#index"
   
-  get "/writers" => "writers#index", :as => :writers
-  get "/writer/:id" => "writers#show", :as => :writer
-  get "/writer/:id.pdf" => "writers#show", :as => :writer, :format => :pdf
-  
+  resources :writers
   
   get "/events" => "events#index", :as => :events
   get "/event/:id" => "events#show", :as => :event
   post "/event/:id/toggle_image" => "events#toggle_image", :as => :event_toggle_image
   
-  resource :bookings do |booking|
+  resources :bookings do 
     member do
       put "toggle_submitted" => "bookings#toggle_submitted", :as => :toogle_submitted
     end
